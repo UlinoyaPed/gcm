b5da1e9f5eed795be47f660b0c85d697480a5624
UlinoyaPed <l15010104769@outlook.com>
2023-02-09 16:48:38 +0800

    ():

--- commit/header.go
+++ commit/header.go
@@ -2,8 +2,12 @@ package commit
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/AlecAivazis/survey/v2"
+
+	"bytes"
+	"log"
 )
 
 func Header() {
@@ -31,4 +35,35 @@ func Header() {
 	fmt.Println(Header + Scope)
 
 	var Subject string
+	inputSubject := &survey.Input{
+		Message: "输入简短描述:",
+	}
+	survey.AskOne(inputSubject, &Subject)
+	fmt.Println(Subject)
+
+	FullCommitMessage := fmt.Sprintf("%s(%s): %s", Header, Scope, Subject)
+
+	// cmd := exec.Command("git commit", "-m", FullCommitMessage)
+	// out, _ := cmd.CombinedOutput()
+	// fmt.Println(out)
+
+	ExecCommand("git commit", "-m"+FullCommitMessage)
+}
+
+func ExecCommand(name string, args ...string) {
+	cmd := exec.Command(name, args...) // 拼接参数与命令
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	var err error
+
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if err = cmd.Run(); err != nil {
+		log.Println(err)
+	}
+
+	fmt.Print(stdout.String())
+	fmt.Print(stderr.String())
 }
