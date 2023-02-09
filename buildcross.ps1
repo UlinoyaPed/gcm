0c360eac505c6360396351807d8f118937ec4273
UlinoyaPed <l15010104769@outlook.com>
2023-02-09 18:21:25 +0800

    fix(script): 修改ps1脚本未安装gox错误

--- buildcross.ps1
+++ buildcross.ps1
@@ -1,5 +1,6 @@
 function main {
 	$latestTag = git describe --tags
+	go install github.com/mitchellh/gox@latest
 	gox -output="./releases/{{.Dir}}_{{.OS}}_{{.Arch}}_$($latestTag)" 
     Set-Location $PSScriptRoot
 }
